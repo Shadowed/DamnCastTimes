e6c1f642bf8a5406d20fe2085069d6e1eb6d95ba
Shadowed <shadowed.wow@gmail.com>
2007-09-30 19:53:37 +0000

    * Fixed cast timer trying to show for non-player casts

--- DamnCastTimes.lua
+++ DamnCastTimes.lua
@@ -24,6 +24,10 @@ local orig_CastingBarFrame_OnUpdate = CastingBarFrame_OnUpdate
 function CastingBarFrame_OnUpdate(...)
 	orig_CastingBarFrame_OnUpdate(...)
 	
+	if( CastingBarFrame.unit ~= "player" ) then
+		return
+	end
+	
 	if( this.casting ) then
 		if( not this.spellPushback ) then
 			DCT.castTimeText:SetText(format(spellFormat, CastingBarFrame.maxValue - GetTime()))
@@ -46,7 +50,7 @@ end
 
 local orig_CastingBarFrame_OnEvent = CastingBarFrame_OnEvent
 function CastingBarFrame_OnEvent(event, unit, ...)
-	if( event == "UNIT_SPELLCAST_DELAYED" ) then
+	if( event == "UNIT_SPELLCAST_DELAYED" and unit == "player" ) then
 		local name, _, _, _, startTime, endTime = UnitCastingInfo(CastingBarFrame.unit);
 		if( not name ) then
 			return
